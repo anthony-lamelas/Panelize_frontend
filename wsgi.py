f2eeb9b7d54a0843ae6200b7d5d07868b7d31003
anthony-lamelas <anthony.lamelas23@gmail.com>
2025-04-11 14:44:26 -0400

    backend setup

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+# Web Server Gateway Interface
+# WSGI is a specification that describes how a web server communicates with web applications
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
